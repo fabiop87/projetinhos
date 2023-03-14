70bb61773834f623345f1a8b67a180174a1369bc
Fabio Pilon <100612310+fabiop87@users.noreply.github.com>
2023-03-14 00:43:03 -0300

    add recomendações (1)

--- Sistema Eventos/tabelas.sql
+++ Sistema Eventos/tabelas.sql
@@ -3,14 +3,14 @@ USE eventosfaculdade;
 
 CREATE TABLE eventos (
     idEvento INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    nomeEvento VARCHAR(50) NOT NULL,
-    local VARCHAR(50),
+    nomeEvento VARCHAR(100) NOT NULL,
+    local VARCHAR(100),
     dataEvento DATE,
     horarioInicio TIME,
     horarioTermino TIME,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
-
+-- Os eventos de mais de um dia poderão ser colocados um dia em cada (parte 1, parte2 ...) ou (colocar outra ideia)
 
 CREATE TABLE cursos (
     idCurso INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -20,18 +20,18 @@ CREATE TABLE cursos (
 
 CREATE TABLE coordenadores (
     idCoordenador INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(50) NOT NULL UNIQUE,
+    nome VARCHAR(100) NOT NULL UNIQUE,
     idCurso int NOT NULL,
-    senha VARCHAR(80) NOT NULL,
+    senha VARCHAR(80) NOT NULL, -- password hash 
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (idCurso) REFERENCES Cursos(idCurso)
 );
 
 CREATE TABLE alunos (
     idAluno INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(50) NOT NULL,
+    nome VARCHAR(100) NOT NULL,
     ra VARCHAR(7) NOT NULL,
-    idCurso int NOT NULL,
+    idCurso int NOT NULL,  -- form -> value do select == idCurso 
     senha VARCHAR(80) NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (idCurso) REFERENCES Cursos(idCurso)
@@ -40,9 +40,13 @@ CREATE TABLE alunos (
 CREATE TABLE presenca (
     idEvento INT NOT NULL,
     idAluno INT NOT NULL,
-    codigoCoord VARCHAR(10),
-    codigoAluno VARCHAR(10),
+    codigoCoord VARCHAR(25),
+    codigoAluno VARCHAR(25),
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
+                     ON UPDATE CURRENT_TIMESTAMP,
+  KEY (updated_at),
+    qtdTentativas TINYINT unsigned DEFAULT 0,
     PRIMARY KEY (idEvento, idAluno),
     CONSTRAINT fk_evt FOREIGN KEY (idEvento) REFERENCES eventos(idEvento),
     CONSTRAINT fk_aln FOREIGN KEY (idAluno) REFERENCES alunos(idAluno)
