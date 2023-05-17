51ce2a2c16266ea7e2a382804c2cb303ba9953e3
Fabio Pilon <100612310+fabiop87@users.noreply.github.com>
2023-05-16 21:28:13 -0300

    Delete P2Fabio directory

--- P2Fabio/resources/jhonson.js
+++
@@ -1,9 +0,0 @@
-
-function pedirConfirmacao() {
-    if (confirm("Tem certeza que deseja fazer isso ?")) {
-      return true; // o formulário será enviado para o arquivo PHP
-    } else {
-      return false; // o formulário não será enviado
-    }
-  }
-    
